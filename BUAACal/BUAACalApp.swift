feda079cbd5c7c6246ef8dd7b1ff8df9356407b1
roife <roifewu@gmail.com>
2020-10-03 18:01:17 +0800

    Initial Commit

---
+++ BUAACal/BUAACalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BUAACalApp.swift
+//  BUAACal
+//
+//  Created by 吴家焱 on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct BUAACalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BUAACal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BUAACal
+//
+//  Created by 吴家焱 on 10/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
